65a717291727c4adf5cfa5832a2dc211a80af1b4
ascutariu <andreiscutariu92@gmail.com>
2019-07-30 18:35:27 +0300

    list vs hashset

---
+++ Performance/ListVsHashSet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ListVsHashSet
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var random = new Random();
+
+            // TODO - 0 check the search of stringToSearch
+
+            var list = new List<string>();
+            var hashSet = new HashSet<string>();
+
+            var s1 = new Stopwatch();
+            s1.Start();
+            for (var x = 0; x < 2000000; x++)
+            {
+                list.Add("string_" + random.Next());
+            }
+            Console.WriteLine($"insert into list {s1.ElapsedMilliseconds} ms");
+
+            var s2 = new Stopwatch();
+            s2.Start();
+            for (var x = 0; x < 2000000; x++)
+            {
+                hashSet.Add("string_" + random.Next());
+            }
+            Console.WriteLine($"insert into hashset {s2.ElapsedMilliseconds} ms");
+
+            var stringToSearch = "string_22";
+
+            // TODO - 1 check the search of stringToSearch
+
+            // TODO - 2 check the delete of stringToSearch
+        }
+    }
+}
