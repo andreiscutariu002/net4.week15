7a43ca3d40d1ecfac44a34e4a2fd5ed747d47320
ascutariu <andreiscutariu92@gmail.com>
2019-07-30 20:42:43 +0300

    updates

---
+++ Performance/WordCounter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    class Program
+    {
+        private const string FileName = "DataSimple.txt";
+
+        static void Main(string[] args)
+        {
+            var line = File.ReadLines(FileName);
+            Console.WriteLine(line.First());
+        }
+    }
+}
